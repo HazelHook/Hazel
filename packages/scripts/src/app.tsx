9d5ae7a99793f2782bdfe3d79bcf55b3337a8afb
Makisuo <31933546+Makisuo@users.noreply.github.com>
2023-06-27 12:42:02 +0200

    Bump packages

--- packages/scripts/src/app.tsx
+++ packages/scripts/src/app.tsx
@@ -1,5 +1,4 @@
-import React from "react"
-import { Box, Text } from "./ext/ink"
+import { Box } from "./ext/ink"
 import Gradient from "./ext/ink-gradient"
 import BigText from "./ext/ink-big-text"
 import Options from "./options/options.js"
@@ -7,13 +6,20 @@ import Options from "./options/options.js"
 export default function App() {
 	return (
 		<Box width="100%" display="flex" flexDirection="column">
-			<Box borderColor='#CC671B' borderStyle='round' width={'100%'} alignItems='center' display='flex' justifyContent='center'>
+			<Box
+				borderColor="#CC671B"
+				borderStyle="round"
+				width={"100%"}
+				alignItems="center"
+				display="flex"
+				justifyContent="center"
+			>
 				<Gradient name="fruit">
-					<BigText text="Hazlenut Planter" font="simple" space={false}/>
-					{'\n'}
+					<BigText text="Hazlenut Planter" font="simple" space={false} />
+					{"\n"}
 				</Gradient>
 			</Box>
-			<Options/>
+			<Options />
 		</Box>
 	)
 }
