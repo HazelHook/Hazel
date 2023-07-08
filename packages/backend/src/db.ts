ffe4c6e776ef73a90be0294ddb366fb4be59fc6c
Makisuo <31933546+Makisuo@users.noreply.github.com>
2023-07-08 10:38:37 +0200

    chore: cleanup

--- packages/backend/src/db.ts
+++
@@ -1,9 +0,0 @@
-import { connectWDB } from "db/src"
-
-export const cDb = (input: {
-	host: string
-	username: string
-	password: string
-}) => {
-	return connectWDB({ ...input, fetch })
-}
--- packages/backend/src/wasm/transformation.ts
+++
@@ -1,8 +0,0 @@
-// export async function fluxTransformConnection(conn: SelectConnection, input: string): Promise<string> {
-
-// 	return HazelFlux.transform({
-// 		data: input,
-// 		config: conn.fluxConfig,
-// 		mode: process.env.NODE_ENV === "production" ? "prod" : "dev",
-// 	})
-// }
