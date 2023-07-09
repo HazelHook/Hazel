8dc9719feee1974daa7a8027f94203238218c274
Jeremy Funk <jeremy@kombo.dev>
2023-07-09 14:25:01 +0200

    Fix bugs

--- packages/db/src/tinybird/model/tiny-request.ts
+++ packages/db/src/tinybird/model/tiny-request.ts
@@ -3,16 +3,12 @@ import { z } from "zod"
 import { TinybirdView, body, hazelVersion, headers, period, successState, timestamp } from "../zod-common"
 import { Tinybird } from "@chronark/zod-bird"
 
-const ids: Record<string, z.ZodTypeAny> = {
+export const requestEventSchema = z.object({
+	// IDs
 	id: z.string(),
 	customer_id: z.string(),
 	source_id: z.string(),
 	request_id: z.string(),
-}
-
-export const requestEventSchema = z.object({
-	// IDs
-	...ids,
 
 	// Metadata
 	version: hazelVersion,
@@ -36,20 +32,22 @@ export const buildTinyBirdRequest = (tb: Tinybird) => {
 		schema: requestEventSchema,
 		tb,
 		requestParameters: z.object({
-			...ids,
+			id: z.string().optional(),
+			customer_id: z.string().optional(),
+			source_id: z.string().optional(),
+			request_id: z.string().optional(),
 		
+			success: z.number().optional(),
+			start_date: z.date().optional(),
+			end_date: z.date().optional(),
+
 			limit: z.number().optional(),
 			offset: z.number().optional(),
 		}),
 		kpisSchema: z.object({
 			customer_id: z.string(),
-			source_id: z.string().optional(),
-		
-			success: z.number().optional(),
-		
-			period: period.default("daily").optional(),
-			start_date: z.date(),
-			end_date: z.date().optional(),
+			events: z.number(),
+			date: z.date()
 		}),
 		timeseriesSchema: z
 		.object({
--- packages/db/src/tinybird/model/tiny-response.ts
+++ packages/db/src/tinybird/model/tiny-response.ts
@@ -3,17 +3,13 @@ import { z } from "zod"
 import { TinybirdView, body, hazelVersion, headers, period, successState, timestamp } from "../zod-common"
 import { Tinybird } from "@chronark/zod-bird"
 
-const ids: Record<string, z.ZodTypeAny> = {
+export const responseEventSchema = z.object({
+	// IDs
 	id: z.string(),
 	customer_id: z.string(),
 	source_id: z.string(),
 	destination_id: z.string(),
 	request_id: z.string(),
-}
-
-export const responseEventSchema = z.object({
-	// IDs
-	...ids,
 
 	// Metadata
 	version: hazelVersion,
@@ -38,7 +34,16 @@ export const buildTinyBirdResponse = (tb: Tinybird) => {
 		schema: responseEventSchema,
 		tb,
 		requestParameters: z.object({
-			...ids,
+			id: z.string().optional(),
+			customer_id: z.string().optional(),
+			source_id: z.string().optional(),
+			destination_id: z.string().optional(),
+			request_id: z.string().optional(),
+
+			success: z.number().optional(),
+			start_date: z.date().optional(),
+			end_date: z.date().optional(),
+			
 		
 			limit: z.number().optional(),
 			offset: z.number().optional(),
