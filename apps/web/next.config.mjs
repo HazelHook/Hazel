62ed5b2cd6e2f35d67620a96d25972e6e211ab27
Makisuo <31933546+Makisuo@users.noreply.github.com>
2023-07-10 08:31:11 +0200

    feat: add template docs

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -3,6 +3,7 @@ const nextConfig = {
 	reactStrictMode: true,
 	experimental: {
 		serverActions: true,
+		serverComponentsExternalPackages: ["@trpc/server"],
 	},
 	transpilePackages: ["ui"],
 	webpack: (config) => {
