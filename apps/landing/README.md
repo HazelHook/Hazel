99f5d13741a35f86f55d747962d30d9d3cd5d311
Makisuo <31933546+Makisuo@users.noreply.github.com>
2023-07-12 20:15:42 +0200

    feat: update

--- apps/landing/README.md
+++ apps/landing/README.md
@@ -1,11 +1,13 @@
 # Astro & Tailwind CSS Starter Kit
 
 ## Template Integrations
+
 - @astrojs/tailwind - https://docs.astro.build/en/guides/integrations-guide/tailwind/
 - @astrojs/image - https://docs.astro.build/en/guides/integrations-guide/image/
 - @astrojs/sitemap - https://docs.astro.build/en/guides/integrations-guide/sitemap/
 - Astrolib SEO - https://github.com/onwidget/astrolib/tree/main/packages/seo
 - Astro compress - https://github.com/NikolaRHristov/astro-compress#readme
+
 ## Template Structure
 
 Inside of your Astro project, you'll see the following folders and files:
--- apps/landing/astro.config.mjs
+++ apps/landing/astro.config.mjs
@@ -1,24 +1,23 @@
-import { defineConfig } from "astro/config"
-import tailwind from "@astrojs/tailwind"
-import image from "@astrojs/image"
-import compress from "astro-compress"
-import sitemap from "@astrojs/sitemap"
-import mdx from "@astrojs/mdx"
-
-import react from "@astrojs/react"
+import image from "@astrojs/image";
+import mdx from "@astrojs/mdx";
+import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+import compress from "astro-compress";
+import { defineConfig } from "astro/config";
 
 export default defineConfig({
-	markdown: {
-		drafts: true,
-		shikiConfig: {
-			theme: "css-variables",
-		},
-	},
-	shikiConfig: {
-		wrap: true,
-		skipInline: false,
-		drafts: true,
-	},
-	site: "https://hazelhook.dev",
-	integrations: [tailwind(), image(), compress(), sitemap(), mdx(), react()],
-})
+  markdown: {
+    drafts: true,
+    shikiConfig: {
+      theme: "css-variables",
+    },
+  },
+  shikiConfig: {
+    wrap: true,
+    skipInline: false,
+    drafts: true,
+  },
+  site: "https://hazelhook.dev",
+  integrations: [tailwind(), image(), compress(), sitemap(), mdx(), react()],
+});
