53c758f216f14823a43a9fb0fcdcedb981ea835e
Makisuo <31933546+Makisuo@users.noreply.github.com>
2023-07-15 21:05:14 +0200

    xd

--- apps/web/lib/orm.ts
+++ apps/web/lib/orm.ts
@@ -1,11 +1,11 @@
+import { cache } from "react"
 import { notFound } from "next/navigation"
 
 import { PromiseType } from "@/lib/ts/helpers"
 
 import db from "./db"
-import { unstable_cache } from "next/cache"
 
-export const getCachedConnection = unstable_cache(async ({ publicId }: { publicId: string }) => {
+export const getCachedConnection = cache(async ({ publicId }: { publicId: string }) => {
 	const connection = await db.connection.getOne({ publicId })
 
 	if (!connection) {
@@ -16,7 +16,7 @@ export const getCachedConnection = unstable_cache(async ({ publicId }: { publicI
 })
 export type CacheConnection = PromiseType<ReturnType<typeof getCachedConnection>>
 
-export const getCachedSource = unstable_cache(
+export const getCachedSource = cache(
 	async ({
 		publicId,
 		redirectMissing = true,
@@ -39,7 +39,7 @@ export const getCachedSource = unstable_cache(
 )
 export type CacheSource = PromiseType<ReturnType<typeof getCachedSource>>
 
-export const getCachedDestination = unstable_cache(async ({ publicId }: { publicId: string }) => {
+export const getCachedDestination = cache(async ({ publicId }: { publicId: string }) => {
 	const destination = await db.destination.getOne({ publicId })
 
 	if (!destination) {
@@ -50,7 +50,7 @@ export const getCachedDestination = unstable_cache(async ({ publicId }: { public
 })
 export type CacheDestination = PromiseType<ReturnType<typeof getCachedDestination>>
 
-export const getCachedIntegrations = unstable_cache(async ({ customerId }: { customerId: string }) => {
+export const getCachedIntegrations = cache(async ({ customerId }: { customerId: string }) => {
 	const integration = await db.integration.getMany({ customerId })
 
 	if (!integration) {
