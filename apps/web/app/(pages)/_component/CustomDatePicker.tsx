26e5969d5ad3b7320efc18fcb3c94e231fbf0ae7
Makisuo <31933546+Makisuo@users.noreply.github.com>
2023-08-11 17:38:23 +0200

    chore: update dependencies

--- apps/web/app/(pages)/_component/CustomDatePicker.tsx
+++ apps/web/app/(pages)/_component/CustomDatePicker.tsx
@@ -1,5 +1,3 @@
-"use client"
-
 import { useState } from "react"
 import { subDays } from "date-fns"
 import { DateRange } from "react-day-picker"
--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,3 +1,10 @@
+import million from "million/compiler"
+
+const millionConfig = {
+	// if you're using RSC:
+	auto: { rsc: true },
+}
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
