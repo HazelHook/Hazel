5990660a995110e6f48234c0d0136b8a649749f5
Makisuo <31933546+Makisuo@users.noreply.github.com>
2023-10-08 23:50:44 +0200

    feat: rework auth flow to use auto form

--- apps/web/app/[locale]/auth/components/EmailPasswordSignInForm.tsx
+++ apps/web/app/[locale]/auth/components/EmailPasswordSignInForm.tsx
@@ -1,70 +1,59 @@
 "use client"
 
-import { useForm } from "react-hook-form"
 import Link from "next/link"
-import { Label } from "@/components/ui/label"
-import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
 import If from "@/components/ui/if"
 import { useI18n } from "@/i18n/client"
+import { z } from "zod"
+import AutoForm from "@/components/ui/auto-form"
+import { passwordSchema } from "./EmailPasswordSignUpForm"
+
+const emailPasswordSignInFormSchema = z.object({
+	email: z.string().email(),
+	password: passwordSchema,
+})
+
+type Schema = z.infer<typeof emailPasswordSignInFormSchema>
 
 const EmailPasswordSignInForm: React.FCC<{
-	onSubmit: (params: { email: string; password: string }) => unknown
+	onSubmit: (params: Schema) => void
 	loading: boolean
 }> = ({ onSubmit, loading }) => {
 	const t = useI18n()
-	const { register, handleSubmit } = useForm({
-		defaultValues: {
-			email: "",
-			password: "",
-		},
-	})
-
-	const emailControl = register("email", { required: true })
-	const passwordControl = register("password", { required: true })
 
 	return (
-		<form className={"w-full"} onSubmit={handleSubmit(onSubmit)}>
-			<div className={"flex-col space-y-4"}>
-				<div className="space-y-1">
-					<Label>{t("common.emailAddress")}</Label>
-
-					<Input
-						data-cy={"email-input"}
-						required
-						type="email"
-						placeholder={"your@email.com"}
-						{...emailControl}
-					/>
-				</div>
-
-				<div className="space-y-1">
-					<Label>{t("common.password")}</Label>
-
-					<Input required data-cy={"password-input"} type="password" placeholder={""} {...passwordControl} />
-
-					<div className={"py-0.5 text-xs"}>
-						<Link href={"/auth/password-reset"} className={"hover:underline"}>
+		<AutoForm
+			className="w-full"
+			onSubmit={async (v) => {
+				onSubmit(v)
+			}}
+			formSchema={emailPasswordSignInFormSchema}
+			fieldConfig={{
+				email: {
+					inputProps: {
+						placeholder: "your@email.com",
+						type: "email",
+					},
+				},
+				password: {
+					inputProps: {
+						required: true,
+						type: "password",
+					},
+					description: (
+						<Link href={"/auth/password-reset"} className={"text-xs text-foreground hover:underline"}>
 							{t("auth.passwordForgottenQuestion")}
 						</Link>
-					</div>
-				</div>
-
-				<div>
-					<Button
-						className={"w-full"}
-						color={"primary"}
-						data-cy="auth-submit-button"
-						type="submit"
-						loading={loading}
-					>
-						<If condition={loading} fallback={t("auth.signIn")}>
-							{t("auth.signingIn")}
-						</If>
-					</Button>
-				</div>
-			</div>
-		</form>
+					),
+				},
+			}}
+		>
+			<Button className={"w-full"} color={"primary"} data-cy="auth-submit-button" type="submit" loading={loading}>
+				<If condition={loading} fallback={t("auth.signIn")}>
+					{t("auth.signingIn")}
+				</If>
+			</Button>
+		</AutoForm>
 	)
 }
 
--- apps/web/app/[locale]/auth/components/EmailPasswordSignUpForm.tsx
+++ apps/web/app/[locale]/auth/components/EmailPasswordSignUpForm.tsx
@@ -1,122 +1,86 @@
+import AutoForm from "@/components/ui/auto-form"
 import { Button } from "@/components/ui/button"
 import If from "@/components/ui/if"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
 import { useI18n } from "@/i18n/client"
-import { useForm } from "react-hook-form"
+import { z } from "zod"
+
+export const passwordSchema = z
+	.string()
+	.min(6, "Password must be at least 6 characters long")
+	.refine((password) => /[A-Z]/.test(password), "Password must contain at least one uppercase letter")
+	.refine((password) => /[a-z]/.test(password), "Password must contain at least one lowercase letter")
+	.refine((password) => /\d/.test(password), "Password must contain at least one number")
+	.refine(
+		(password) => /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/.test(password),
+		"Password must contain at least one special character",
+	)
+	.refine((password) => !/\s/.test(password), "Password must not contain spaces")
+	.refine(
+		(password) => !["password123", "welcome"].includes(password),
+		"Password contains a forbidden word or sequence",
+	)
+
+const emailPasswordSignUpFormSchema = z
+	.object({
+		email: z.string().email(),
+		password: passwordSchema,
+		repeatPassword: z.string(),
+	})
+	.refine((data) => data.password === data.repeatPassword, {
+		message: "Passwords must match",
+		path: ["repeatPassword"],
+	})
+
+type Schema = z.infer<typeof emailPasswordSignUpFormSchema>
 
 const EmailPasswordSignUpForm: React.FCC<{
-	onSubmit: (params: {
-		email: string
-		password: string
-		repeatPassword: string
-	}) => unknown
+	onSubmit: (params: Schema) => void
 	loading: boolean
 }> = ({ onSubmit, loading }) => {
 	const t = useI18n()
 
-	const { register, handleSubmit, watch, formState } = useForm({
-		defaultValues: {
-			email: "",
-			password: "",
-			repeatPassword: "",
-		},
-	})
-
-	const emailControl = register("email", { required: true })
-	const errors = formState.errors
-
-	const passwordControl = register("password", {
-		required: true,
-		minLength: {
-			value: 6,
-			message: t("auth.passwordLengthError"),
-		},
-	})
-
-	const passwordValue = watch("password")
-
-	const repeatPasswordControl = register("repeatPassword", {
-		required: true,
-		minLength: {
-			value: 6,
-			message: t("auth.passwordLengthError"),
-		},
-		validate: (value) => {
-			if (value !== passwordValue) {
-				return t("auth.passwordsDoNotMatch")
-			}
-
-			return true
-		},
-	})
-
 	return (
-		<form className={"w-full"} onSubmit={handleSubmit(onSubmit)}>
-			<div className={"flex-col space-y-4"}>
-				<div className="space-y-1">
-					<Label>{t("common.emailAddress")}</Label>
-
-					<Input
-						{...emailControl}
-						data-cy={"email-input"}
-						required
-						type="email"
-						placeholder={"your@email.com"}
-					/>
-
-					<p className="text-sm font-medium text-destructive">{errors.email?.message}</p>
-				</div>
-
-				<div className="space-y-1">
-					<Label>{t("common.password")}</Label>
-
-					<Input {...passwordControl} data-cy={"password-input"} required type="password" placeholder={""} />
-
-					<span className={"block pl-1 text-xs font-normal leading-tight text-muted-foreground"}>
-						{t("auth.passwordHint")}
-					</span>
-
-					<p className="text-sm font-medium text-destructive" data-cy="password-error">
-						{errors.password?.message}
-					</p>
-				</div>
-
-				<div className="space-y-1">
-					<Label>{t("auth.repeatPassword")}</Label>
-
-					<Input
-						{...repeatPasswordControl}
-						data-cy={"repeat-password-input"}
-						required
-						type="password"
-						placeholder={""}
-					/>
-
-					<span className={"block pl-1 text-xs font-normal leading-tight text-muted-foreground"}>
-						{t("auth.repeatPasswordHint")}
-					</span>
-
-					<p className="text-sm font-medium text-destructive" data-cy="repeat-password-error">
-						{errors.repeatPassword?.message}
-					</p>
-				</div>
-
-				<div>
-					<Button
-						data-cy={"auth-submit-button"}
-						className={"w-full"}
-						color={"primary"}
-						type="submit"
-						loading={loading}
-					>
-						<If condition={loading} fallback={t("auth.getStarted")}>
-							{t("auth.signingUp")}
-						</If>
-					</Button>
-				</div>
-			</div>
-		</form>
+		<AutoForm
+			className="w-full"
+			formSchema={emailPasswordSignUpFormSchema}
+			onSubmit={async (v) => {
+				onSubmit(v)
+			}}
+			fieldConfig={{
+				email: {
+					inputProps: {
+						placeholder: "your@email.com",
+						type: "email",
+					},
+				},
+				password: {
+					description: t("auth.passwordHint"),
+					inputProps: {
+						required: true,
+						type: "password",
+					},
+				},
+				repeatPassword: {
+					description: t("auth.repeatPasswordHint"),
+					inputProps: {
+						required: true,
+						type: "password",
+					},
+				},
+			}}
+		>
+			<Button
+				data-cy={"auth-submit-button"}
+				className={"w-full"}
+				color={"primary"}
+				type="submit"
+				loading={loading}
+			>
+				<If condition={loading} fallback={t("auth.getStarted")}>
+					{t("auth.signingUp")}
+				</If>
+			</Button>
+		</AutoForm>
 	)
 }
 
