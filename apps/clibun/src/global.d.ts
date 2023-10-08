e52d3ac8a2df8ab69016a96b76e2a5990a7463bd
Makisuo <31933546+Makisuo@users.noreply.github.com>
2023-10-08 22:07:41 +0200

    remove clibun app

--- apps/clibun/src/global.d.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from "./modules/module.tsx"
-
-declare global {
-	// rome-ignore lint/style/noVar: <explanation>
-	var hazelModule: Module
-}
-export default global
--- apps/clibun/src/modules/index.ts
+++
@@ -1,6 +0,0 @@
-import { BlessedModule } from "./blessed/index.js"
-import { Module } from "./module.js"
-
-export function getModule(): Module {
-	return BlessedModule.create()
-}
