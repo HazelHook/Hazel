379f8834fd8e9b74beef26fb421cdc854f47067f
Makisuo <31933546+Makisuo@users.noreply.github.com>
2023-11-11 21:52:35 +0100

    fix

--- packages/ui/src/data-table.tsx
+++ packages/ui/src/data-table.tsx
@@ -1,4 +1,3 @@
 export * from "./data-table/simple-data-table"
-export * from "./data-table/data-table"
 export * from "./data-table/advanced-data-table"
 export * from "./data-table/data-table-pagination"
